40990ac8343faedba49889d44ea9851f6516a622
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2023-07-15 11:14:08 +0700

    fix: fix

--- src/example.test.tsx
+++ src/example.test.tsx
@@ -1,14 +1,14 @@
 import { it, expect } from "@jest/globals";
 import { render } from "@testing-library/react";
 import events from "@testing-library/user-event";
-//
+
 import { BrowserRouter } from "react-router-dom";
 import { MemoryRouter } from "react-router";
 import { Provider } from "react-redux";
 
 import { initStore } from "./store";
 import { Application } from "./Application";
-//
+
 it('по адресу /about должна открываться страница "о проекте"', () => {
   const store = initStore();
   const application = (
